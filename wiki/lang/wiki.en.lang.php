bcd3ef5424968b7dd1d8c701f9dfcddaa1af51cb
xerora <tyler@xaez.org>
2014-07-15 15:08:56 -0700

    Language changes

--- wiki/lang/wiki.en.lang.php
+++ wiki/lang/wiki.en.lang.php
@@ -5,7 +5,13 @@ $L['Wiki'] = 'Wiki';
 $L['wiki_revision'] = 'Revision';
 $L['wiki_compare'] = 'Compare';
 
+// Structure configuration titles
 $L['wiki_subcats_0'] = $L['No'];
 $L['wiki_subcats_1'] = $L['Yes'];
 $L['wiki_enabled_0'] = $L['No'];
 $L['wiki_enabled_1'] = $L['Yes'];
+
+
+// Structure configuration descriptions
+$L['structure_wiki_subcats_title'] = 'Enable wiki functionality on all sub-categories of this category ?';
+$L['structure_wiki_enabled_title'] = 'Enable wiki functionality on this category ?';
--- wiki/setup/wiki.install.php
+++ wiki/setup/wiki.install.php
@@ -4,9 +4,9 @@ require_once cot_incfile('page', 'module');
 
 if(!cot::$db->fieldExists(cot::$db->structure, 'structure_wiki_enabled'))
 {
-	cot_extrafield_add(cot::$db->structure, 'wiki_enabled', 'radio', '<label><input type="radio" name="{$name}" value="{$value}"{$checked} /> {$title}</label>', '1,0', '0', false, 'HTML', 'Wiki Enabled ?');
+	cot_extrafield_add(cot::$db->structure, 'wiki_enabled', 'radio', '<label><input type="radio" name="{$name}" value="{$value}"{$checked} /> {$title}</label>', '1,0', '0', false, 'HTML');
 }
 if(!cot::$db->fieldExists(cot::$db->structure, 'structure_wiki_subcats'))
 {
-	cot_extrafield_add(cot::$db->structure, 'wiki_subcats', 'radio', '<label><input type="radio" name="{$name}" value="{$value}"{$checked} /> {$title}</label>', '1,0', '0', false, 'HTML', 'Wiki Sub cat ?');
+	cot_extrafield_add(cot::$db->structure, 'wiki_subcats', 'radio', '<label><input type="radio" name="{$name}" value="{$value}"{$checked} /> {$title}</label>', '1,0', '0', false, 'HTML');
 }
